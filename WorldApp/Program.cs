da7be036981ac2409bfdde4c51c3e632f5c3c401
escaicedo <escaicedosanz@gmail.com>
2019-04-10 17:38:52 -0400

    Initial Commit WorldApp

---
+++ WorldApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
