a1859b87e9a3cd22b9305cdf96473808addc9046
escaicedo <escaicedosanz@gmail.com>
2019-04-10 17:46:53 -0400

    Changed the output message to Hello Universe

--- WorldApp/Program.cs
+++ WorldApp/Program.cs
@@ -6,7 +6,7 @@ namespace WorldApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Universe!");
         }
     }
 }
